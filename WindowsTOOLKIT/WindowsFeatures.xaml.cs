15f90f2ba931157369a393464af470fc48f3d118
Michał <michalOzdz@icloud.com>
2025-05-23 16:22:47 +0200

    Refactor WindowsFeatures.xaml.cs for improved feature state handling and UI responsiveness

--- WindowsTOOLKIT/WindowsFeatures.xaml.cs
+++ WindowsTOOLKIT/WindowsFeatures.xaml.cs
@@ -2,22 +2,16 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace WindowsTOOLKIT
 {
     public partial class WindowsFeatures : Window
     {
-        private List<(string Name, string State)> _featuresBefore = new List<(string Name, string State)>();
+        private List<(string Name, string State)>
+            _featuresBefore = new List<(string Name, string State)>(); // stan funkcji po pobrnaiu DISMEM
 
         public WindowsFeatures()
         {
@@ -27,6 +21,7 @@ namespace WindowsTOOLKIT
         private async void winWindowsFeatures_Loaded(object sender, RoutedEventArgs e)
         {
             WFBTNsave.IsEnabled = false;
+            WFBTNback.IsEnabled = false;
             var loading = new Label
             {
                 Content = "Wczytywanie...",
@@ -34,7 +29,6 @@ namespace WindowsTOOLKIT
             };
 
             GFeatures.Children.Add(loading);
-            //string output = "";
             string output = await Task.Run(() =>
                 // await, by zdazyla sie pokazac informacja o wczytywaniu, na innym watku sie to wykona!!! (notatka bo pewnie zapomne co to robi)
             {
@@ -53,27 +47,31 @@ namespace WindowsTOOLKIT
                 proc.Start();
                 string temp = proc.StandardOutput.ReadToEnd();
                 proc.WaitForExit();
-
                 return temp;
             });
 
             GFeatures.Children.Remove(loading);
 
             bool show = false;
-
             var lines = output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
 
             foreach (var line in lines)
             {
                 if (!(line.StartsWith("Feature Name") || line.StartsWith("---") ||
-                      line == "The operation completed successfully.") && show)
+                      line == "The operation completed successfully.") && show) // wykluczenie, by zostaly same funkcje
                 {
                     var parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                     if (parts.Length >= 2)
                     {
                         string name = parts[0];
-                        string state = parts[parts.Length - 1];
+                        string state = null;
+                        int counter = 1;
+                        while (state != "Enabled" && state != "Disabled")
+                        {
+                            state = parts[parts.Length - counter];
+                            counter++;
+                        }
+
                         _featuresBefore.Add((name, state));
                     }
                 }
@@ -83,9 +81,10 @@ namespace WindowsTOOLKIT
                 }
             }
 
-            _featuresBefore = _featuresBefore.OrderBy(feature => feature.Name).ToList();
+            _featuresBefore = _featuresBefore.OrderBy(feature => feature.Name).ToList(); // BY bylo alfabetycznie
             int row = 0;
 
+            //wypisywanie tego na ekran
             foreach (var feature in _featuresBefore)
             {
                 GFeatures.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
@@ -113,7 +112,9 @@ namespace WindowsTOOLKIT
 
                 row++;
             }
+
             WFBTNsave.IsEnabled = true;
+            WFBTNback.IsEnabled = true;
         }
 
         private void WFBTNback_click(object sender, RoutedEventArgs e)
@@ -124,15 +125,18 @@ namespace WindowsTOOLKIT
         private async void WFBTNSave_click(object sender, RoutedEventArgs e)
         {
             WFBTNsave.IsEnabled = false;
+            WFBTNback.IsEnabled = false;
             var lblIndex = _featuresBefore.Count - 1;
             var cbIndex = _featuresBefore.Count - 1;
             List<(string Name, string State)> featuresAfter = new List<(string Name, string State)>();
 
+            // Przygotowanie listy do porownania nowego stanu z poprzednim
             for (int i = 0; i < _featuresBefore.Count; i++)
             {
                 featuresAfter.Add((null, null));
             }
 
+            // kasuje elementy, i laczy Labele z checkboxami
             for (int i = GFeatures.Children.Count - 1; i >= 0; i--)
             {
                 var elem = GFeatures.Children[i];
@@ -158,63 +162,34 @@ namespace WindowsTOOLKIT
 
             GFeatures.Children.Add(processing);
 
+            // wlaczanie/wylaczanie funkcji dla tych, ktorych checkboxy zmienily stan
             for (int i = 0; i < featuresAfter.Count; i++)
             {
-                if ((featuresAfter[i].Name == null && featuresAfter[i].State == null) || _featuresBefore[i].State=="Pending" )
+                if ((featuresAfter[i].Name == _featuresBefore[i].Name &&
+                     featuresAfter[i].State != _featuresBefore[i].State) &&
+                    !(featuresAfter[i].Name == null || featuresAfter[i].State == null))
                 {
-                    continue;
-                }
-
-                if (featuresAfter[i].Name == "IIS-HostableWebCore")
-                {
-                    i = i;
-                }
-
-                if (featuresAfter[i].Name == _featuresBefore[i].Name &&
-                    featuresAfter[i].State != _featuresBefore[i].State) {
                     processing.Content = "Zmiana stanu: " + featuresAfter[i].Name;
-                    string output = await Task.Run(() =>
+
+                    await Task.Run(() =>
                     {
-                        if (featuresAfter[i].State == "Enabled")
+                        Process proc = new Process
                         {
-                            Process proc = new Process
+                            StartInfo = new ProcessStartInfo
                             {
-                                StartInfo = new ProcessStartInfo
-                                {
-                                    FileName = "dism",
-                                    Arguments = "/online /enable-feature /NoRestart /featurename:" + featuresAfter[i].Name,
-                                    RedirectStandardOutput = true,
-                                    UseShellExecute = false,
-                                    CreateNoWindow = true
-                                }
-                            };
-
-                            proc.Start();
-                            string temp = proc.StandardOutput.ReadToEnd();
-                            proc.WaitForExit();
-
-                            return "";
-                        }
-                        else
-                        {
-                            Process proc = new Process
-                            {
-                                StartInfo = new ProcessStartInfo
-                                {
-                                    FileName = "dism",
-                                    Arguments = "/online /disable-feature /NoRestart /featurename:" + featuresAfter[i].Name,
-                                    RedirectStandardOutput = true,
-                                    UseShellExecute = false,
-                                    CreateNoWindow = true
-                                }
-                            };
-
-                            proc.Start();
-                            string temp = proc.StandardOutput.ReadToEnd();
-                            proc.WaitForExit();
-
-                            return "";
-                        }
+                                FileName = "dism",
+                                Arguments = "/online /" +
+                                            (featuresAfter[i].State == "Enabled"
+                                                ? "enable-feature"
+                                                : "disable-feature") +
+                                            " /NoRestart /featurename:" + featuresAfter[i].Name,
+                                RedirectStandardOutput = true,
+                                UseShellExecute = false,
+                                CreateNoWindow = true
+                            }
+                        };
+                        proc.Start();
+                        proc.WaitForExit();
                     });
                 }
             }
@@ -223,63 +198,7 @@ namespace WindowsTOOLKIT
             this.Activate();
             MessageBox.Show("Zapisano zmiany. Zrestartuj komputer, aby zmiany zostały zastosowane");
             this.Topmost = false;
-            
             this.Close();
-
-            //     foreach (var child in GFeatures.Children)
-            //     {
-            //         if (child is CheckBox checkbox)
-            //         {
-            //             string featureName = ((Label)GFeatures.Children[GFeatures.Children.IndexOf(checkbox) - 1]).Content
-            //                 .ToString();
-            //             string state = checkbox.IsChecked == true ? "Enabled" : "Disabled";
-            //
-            //             // MessageBox.Show($"Feature: {featureName} - State: {state}");
-            //
-            //             if (_featuresBefore[row].State == state)
-            //             {
-            //                 row++;
-            //                 continue;
-            //             }
-            //
-            //             if (state == "Enabled")
-            //             {
-            //                 Process proc = new Process
-            //                 {
-            //                     StartInfo = new ProcessStartInfo
-            //                     {
-            //                         FileName = "dism",
-            //                         Arguments = $"/online /enable-feature /featurename:{featureName}",
-            //                         RedirectStandardOutput = true,
-            //                         UseShellExecute = false,
-            //                         CreateNoWindow = true
-            //                     }
-            //                 };
-            //                 
-            //                 proc.Start();
-            //                 proc.WaitForExit();
-            //             }
-            //             else
-            //             {
-            //                 Process proc = new Process
-            //                 {
-            //                     StartInfo = new ProcessStartInfo
-            //                     {
-            //                         FileName = "dism",
-            //                         Arguments = $"/online /enable-feature /featurename:{featureName}",
-            //                         RedirectStandardOutput = true,
-            //                         UseShellExecute = false,
-            //                         CreateNoWindow = true
-            //                     }
-            //                 };
-            //                 proc.Start();
-            //                 proc.WaitForExit();
-            //             }
-            //         }
-            //     }
-            //
-            //     MessageBox.Show("Zapisano zmiany. Zrestartuj komputer, aby zmiany zostały zastosowane");
-            // }
         }
     }
 }
