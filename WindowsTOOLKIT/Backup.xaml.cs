1645bc88274334a458014ccc2137af4056f617dd
Michał <michalOzdz@icloud.com>
2025-05-29 20:33:37 +0200

    aaa

--- WindowsTOOLKIT/Backup.xaml.cs
+++ WindowsTOOLKIT/Backup.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Windows;
 
 namespace WindowsTOOLKIT
 {
@@ -7,6 +9,33 @@ namespace WindowsTOOLKIT
         public Backup()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateBackup_Click(object sender, RoutedEventArgs e)
+        {
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "vssadmin",
+                    Arguments = "List shadowstorage",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            string output = proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            if (output.Contains("No items found that satisfy the query."))
+            {
+                MessageBox.Show(output);
+            }
+            
+
+
+
         }
 
         private void BtnBack_OnClick(object sender, RoutedEventArgs e)
